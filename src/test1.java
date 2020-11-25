9f8d0d39b2aaf789174db0c5466ef03e70f4a8c9
Mehmet Ali Gencay <gencayali20@gmail.com>
2020-11-25 20:32:12 +0100

    Yeni Sepete ekle kismi yapildi

--- src/test1.java
+++ src/test1.java
@@ -1,7 +1,8 @@
 public class test1 {
     public static void main(String[] args) {
         System.out.println("Urunleri listele kismi yazildi");
-        System.out.println("Sepete ekle kismi yapildi");
+        System.out.println("Yeni Sepete ekle kismi yapildi");
+        System.out.println("Yeni Sepete ekle 2. kismi yapildi");
 
     }
 }
